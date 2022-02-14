8a9406e7f886e23ec7c31f0f97a8162e100ca542
Mikołaj Strusiński <mikistrus@gmail.com>
2022-02-14 16:42:00 +0100

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,57 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import examplePeople from './initialpeople';
+import styles from './App.module.css';
+
+import List from './components/list';
+import PersonForm, { Person } from './components/PersonForm';
+
+export interface IState{
+  people: {
+    name: string,
+    age: number,
+    url: string,
+    note?: string
+  }[]
+}
 
 function App() {
+  const [people, setPeople] = useState<IState["people"]>(examplePeople);
+  const [adding, setAdding] = useState<boolean>(false);
+
+  const handlePersonAdd = (newPerson: Person):void => {
+    let newPeople = people;
+    newPeople.push(newPerson);
+    setPeople([...newPeople]);
+  }
+
+  const handleDelete = (id: number):void => {
+    let newPeople = people;
+    newPeople.splice(id, 1);
+    setPeople([...newPeople]);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <h1>People invited to my Party!</h1>
+      <div style={{display: "flex", justifyContent: "center", width: "100%"}}>
+        <List 
+          people={people} 
+          setPeople={setPeople}
+          removeFromList={handleDelete}
+        />
+      </div>
+      <div style={{display: "flex", justifyContent: "center", width: "100%"}}>
+        <button 
+          onClick={() => setAdding(adding => !adding)}
+          className={styles.addPersonButton}  
         >
-          Learn React
-        </a>
-      </header>
+          { adding ? "hide adding person form" : "show adding person form" }
+        </button>
+      </div>
+      {
+        adding ? <PersonForm addPerson={handlePersonAdd} /> : null
+      }
     </div>
   );
 }
